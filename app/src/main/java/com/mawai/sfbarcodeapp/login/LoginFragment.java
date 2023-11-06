2a12351330592d3a357a62824ac3cc67c78da780
infotechltdmawai <infotechltdmawai@gmail.com>
2023-11-06 17:49:46 +0530

    initial commit

--- app/src/main/java/com/mawai/sfbarcodeapp/login/LoginFragment.java
+++ app/src/main/java/com/mawai/sfbarcodeapp/login/LoginFragment.java
@@ -70,12 +70,12 @@ public class LoginFragment extends Fragment {
             }
         });
 
-//        if (sessionManager.getBoolean(SessionManager.CHKBOX)) {
-//            binding.setModel(new LoginModel(sessionManager.getString(SessionManager.USER_NAME),sessionManager.getString(SessionManager.PASSWORD),sessionManager.getString(SessionManager.UNIT_NAME)));
-//            binding.savedUsernamePassword.setChecked(true);
-//        } else {
-//            binding.setModel(new LoginModel());
-//        }
+        if (sessionManager.getBoolean(SessionManager.CHKBOX)) {
+            binding.setModel(new LoginModel(sessionManager.getString(SessionManager.USER_NAME),sessionManager.getString(SessionManager.PASSWORD),sessionManager.getString(SessionManager.UNIT_NAME)));
+            binding.savedUsernamePassword.setChecked(true);
+        } else {
+            binding.setModel(new LoginModel());
+        }
 
         spinnerDialogUnitNo = new SpinnerDialog(getActivity(), items, "Select or Search Unit Name", "Close");// With No Animation
         final List<String> datas = new ArrayList<>();
